33a0e7a19faa262ee6023c6e33cd9578f4e8e89f
coshaman <kmcha72@gmail.com>
2023-10-14 15:47:09 +0900

    new homepage

---
+++ js/scripts.js
@@ -0,0 +1,80 @@
+﻿jQuery(function($){
+
+'use strict';
+
+
+    /* ---------------------------------------------- /*
+     * Countdown
+    /* ---------------------------------------------- */
+
+    (function () {
+      // Countdown
+    	// To change date, simply edit: var endDate = January 26, 2016 20:39:00";
+    	$(function() {
+    	  var endDate = "January 26, 2016 20:39:00";
+    	  $('.tk-countdown .row').countdown({
+    		date: endDate,
+    		render: function(data) {
+    		  $(this.el).html('<div><div class="days"><span>' + this.leadingZeros(data.days, 2) + '</span><span>days</span></div><div class="hours"><span>' + this.leadingZeros(data.hours, 2) + '</span><span>hours</span></div></div><div class="tk-countdown-ms"><div class="minutes"><span>' + this.leadingZeros(data.min, 2) + '</span><span>minutes</span></div><div class="seconds"><span>' + this.leadingZeros(data.sec, 2) + '</span><span>seconds</span></div></div>');
+    		}
+    	  });
+    	});	
+    }());
+
+
+    /* ---------------------------------------------- /*
+     * Preloader
+    /* ---------------------------------------------- */
+    
+    (function () {
+        $(window).load(function() {
+            $('#pre-status').fadeOut();
+            $('#st-preloader').delay(350).fadeOut('slow');
+        });
+    }());
+
+
+
+    /* ---------------------------------------------- /*
+     * Ajax Forms
+    /* ---------------------------------------------- */
+
+    (function () {
+        // E-mail validation via regular expression
+        function isValidEmailAddress(emailAddress) {
+          var pattern = new RegExp(/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i);
+          return pattern.test(emailAddress);
+        };
+
+    	// Ajax mailchimp
+        // Example MailChimp url: http://xxx.xxx.list-manage.com/subscribe/post?u=xxx&id=xxx
+        $('#subscribe').ajaxChimp({
+          language: 'es',
+          url: 'http://xxx.xxx.list-manage.com/subscribe/post?u=xxx&id=xxx'
+        });
+
+        // Mailchimp translation
+        //
+        // Defaults:
+        //'submit': 'Submitting...',
+        //  0: 'We have sent you a confirmation email',
+        //  1: 'Please enter a value',
+        //  2: 'An email address must contain a single @',
+        //  3: 'The domain portion of the email address is invalid (the portion after the @: )',
+        //  4: 'The username portion of the email address is invalid (the portion before the @: )',
+        //  5: 'This email address looks fake or invalid. Please enter a real email address'
+
+        $.ajaxChimp.translations.es = {
+          'submit': 'Submitting...',
+          0: '<i class="fa fa-check"></i> We will be in touch soon!',
+          1: '<i class="fa fa-warning"></i> You must enter a valid e-mail address.',
+          2: '<i class="fa fa-warning"></i> E-mail address is not valid.',
+          3: '<i class="fa fa-warning"></i> E-mail address is not valid.',
+          4: '<i class="fa fa-warning"></i> E-mail address is not valid.',
+          5: '<i class="fa fa-warning"></i> E-mail address is not valid.'
+        }
+
+    }());
+
+	
+});
---
+++ mainsystem.js
@@ -0,0 +1,200 @@
+const commandlist = {}
+commandlist['help'] = `
+hello : greetings
+hello -long : introduction of PRON
+help : commandlist
+member -YYYY : member list of the year
+project : our projects
+img : gallery
+
+add -kr for Korean
+`;
+commandlist['help -kr'] = `
+help: print this message and exit
+hello: greetings
+hello -intro: introduction of PRON
+show [image/project] [-Y/—year] year: show images/projects of the given year
+member [-Y/—year]: member list of the year
+
+exit: return to main website
+`;
+commandlist['start'] = `
+`
+commandlist['hello'] = `
+<img src="images/logo.png">
+
+hello!
+This is shinsung high school computer&programming club <u>'PRON'</u>
+
+`
+commandlist['hello -intro'] = `
+hello!
+프론 홈페이지에 오신 여러분을 환영합니다.
+<img src="logo.png">
+`
+commandlist['sudo -rm -rf ./*'] = `
+
+`
+commandlist['exit'] = `
+
+`
+commandlist['안동기'] = `
+기장
+`
+let codelist = [];
+codelist.push(`#include <stdio.h>
+int main(void) {
+    printf("Hello, World!");
+    return 0;
+}`);
+codelist.push(`for(int i=1;i<10;i++){
+    printf("%d\\n",i);
+}`);
+//codelist.push(``);
+
+
+
+function changeme(){
+    if(document.getElementById("finaltest").innerHTML.replace(/&nbsp;/gi," ").length < 120){
+        if(event.keyCode != 32 && event.keyCode != 13){
+            document.getElementById("finaltest").innerHTML += String.fromCharCode(event.keyCode);
+        }
+        document.getElementById("inputplace").value = "";
+    }
+    document.getElementById("jb-content").scrollLeft += 1000000;
+    document.getElementById("inputplace").value = "";
+    
+    
+}
+let step = 0;
+let gamestarted = false;
+let nStart;
+let nEnd;
+let gamestep = 0;
+let gametext;
+let point = 0;
+function commandoutput(command) {
+    console.log(command);
+    if(command == "exit" || command == "sudo -rm -rf ./*"){
+        location.replace('index.html');
+    }
+    else if(command == "start") {
+        nStart = new Date().getTime();
+        //타자게임
+        let textl = codelist[Math.floor(Math.random() * codelist.length)];
+        //testl = textl.replace(/\\/gi,"|_");
+        textl = textl.replace(/\n\n/gi,"&nbsp;");
+        textl = textl.replace(/\\/gi,"\\");
+        gametext = textl.split("\n");
+        step = gametext.length;
+        gamestarted = true;
+        for(gamestep = 0;gamestep < step;gamestep++){
+            gametext[gamestep] = gametext[gamestep].replace(/ /gi,"&nbsp;");
+            gametext[gamestep] = gametext[gamestep].replace(/</gi,"&lt;");
+            gametext[gamestep] = gametext[gamestep].replace(/>/gi,"&gt;");
+        }
+        gamestep = 0;
+        document.getElementById("testfor").innerHTML = "<span style='color: #5184b3'>#</span><span id='finaltest'></span>"
+        document.getElementById("textoutput").innerHTML += "<p>&nbsp;화면에 보이는 문장을 입력 후 엔터를 누르세요.</p>"
+        document.getElementById("textoutput").innerHTML += "<p>&nbsp;"+gametext[gamestep]+"</p>"
+        //nEnd =  new Date().getTime();
+        //var nDiff = nEnd - nStart;
+    }
+    else{
+        let text = commandlist[command];
+        let count = 0;
+        let prepos = 0;
+        let pos = text.indexOf("\n");
+        while (pos !== -1) {
+            count++;
+            document.getElementById("textoutput").innerHTML += "&nbsp;" + text.slice(prepos, pos) + "<br>";
+            prepos = pos;
+            pos = text.indexOf("\n", pos + 1);
+        }
+    }
+    
+    
+}
+function changeme2(){
+    if(event.keyCode == 8){
+        if(document.getElementById("finaltest").innerHTML.slice(-6) == "&nbsp;"){
+            document.getElementById("finaltest").innerHTML = document.getElementById("finaltest").innerHTML.slice(0, -6);
+        }
+        else if(document.getElementById("finaltest").innerHTML.slice(-4) == "&lt;" || document.getElementById("finaltest").innerHTML.slice(-4) == "&gt;"){
+            document.getElementById("finaltest").innerHTML = document.getElementById("finaltest").innerHTML.slice(0, -4);
+        }
+        else{
+            document.getElementById("finaltest").innerHTML = document.getElementById("finaltest").innerHTML.slice(0, -1);
+        }
+        document.getElementById("jb-content").scrollLeft += 10000;
+    }
+    else if(event.keyCode == 32){
+        if(document.getElementById("finaltest").innerHTML.replace(/&nbsp;/gi," ").length < 120){
+            document.getElementById("finaltest").innerHTML += "&nbsp;";
+            document.getElementById("inputplace").value = "";
+            document.getElementById("jb-content").scrollLeft += 10000;
+        }
+    }
+    else if(event.keyCode == 9){
+        if(document.getElementById("finaltest").innerHTML.replace(/&nbsp;/gi," ").length < 120){
+            event.preventDefault();
+            document.getElementById("finaltest").innerHTML += "&nbsp;&nbsp;&nbsp;&nbsp;";
+            document.getElementById("inputplace").value = "";
+            document.getElementById("jb-content").scrollLeft += 10000;
+        }
+    }
+    else if(event.keyCode == 13){
+        
+        if(gamestarted){ //타자게임 공간
+            gamestep += 1;
+            
+            if(gamestep <= step){
+                if(document.getElementById("finaltest").innerHTML == gametext[gamestep - 1]){
+                    point += 500;
+                    document.getElementById("textoutput").innerHTML += "<p style='color : #00f300'>&nbsp;" + document.getElementById("finaltest").innerHTML + "</p>";
+                }
+                else{
+                    document.getElementById("textoutput").innerHTML += "<p style='color : #ef0000'>&nbsp;" + document.getElementById("finaltest").innerHTML + "</p>";
+                }
+                
+            }
+            if(gamestep < step){
+                document.getElementById("textoutput").innerHTML += "<p>&nbsp;" + gametext[gamestep] + "</p>";
+            }
+            else{
+                gamestarted = false;
+                nEnd =  new Date().getTime();
+                var nDiff = (nEnd - nStart) / 1000;
+                alert(nDiff + "초 동안 " + point + "점 득점");
+                document.getElementById("testfor").innerHTML = '<span style="color: #79cd30;">root@pron-pc</span>:~<span style="color: #5184b3">#</span>&nbsp;<span id="finaltest"></span>'
+            }
+            document.getElementById("finaltest").innerHTML = "";
+            document.getElementById("inputplace").value = "";
+            
+        }
+        else{ //명령어 처리 공간
+            document.getElementById("textoutput").innerHTML += '<p><span style="color: #79cd30;">root@pron-pc</span>:~<span style="color: #5184b3">#</span>&nbsp;' + document.getElementById("finaltest").innerHTML + '</p>';
+            let command = document.getElementById("finaltest").innerHTML;
+            document.getElementById("finaltest").innerHTML = "";
+            document.getElementById("inputplace").value = "";
+            command = command.toLowerCase();
+            command = command.replace(/&nbsp;/g, ' ');
+            if(command in commandlist){
+                commandoutput(command);
+                
+            }
+            else{
+                if(command !== ''){
+                document.getElementById("textoutput").innerHTML += "&nbsp;&nbsp;command '" + command + "' is not valid" + "<br>";}
+            }
+        }
+        
+        document.getElementById("jb-content").scrollTop += 10000;
+    }
+    document.getElementById("inputplace").value = "";
+    document.getElementById("jb-content").scrollLeft += 10000;
+}
+
+function byebye(){
+    document.getElementById("inputplace").value = "";
+}
